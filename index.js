699b786120bb8d242a412e12772d0c8d9ebde719
Brian Lee <brian.lee@student.uncommonhs.org>
2017-02-08 22:25:44 +0000

    I Finished the Code

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-
 $(document).ready( function(){
     $('#change-color').click(function(){
-        $('body').css("background-color","red");
-    })
+        var colors = [ "red" , "blue" , "green" , "pink" , "purple"]
+        var randColor = colors[Math.floor(Math.random()*colors.length)];
+        $("body").css("background-color", randColor);
+    });
+    $("#submit-name").click(function(){
+        $("#greeting").html("Hi " + $("#name").val());
+    
+    });
+   $('#pic1').click(function(){
+       $("#gallery-main").attr("src", "https://upload.wikimedia.org/wikipedia/commons/f/f9/Loxodonta_africana_-_old_bull_(Ngorongoro,_2009).jpg");
+   });
+    $('#pic2').click(function(){
+        $("#gallery-main").attr("src", "https://upload.wikimedia.org/wikipedia/commons/6/63/African_elephant_warning_raised_trunk.jpg");
+    });
+    $('#pic3').click(function(){
+         $("#gallery-main").attr("src", "https://iso.500px.com/wp-content/uploads/2014/08/2048-5.jpg");
+    });
 })
 
